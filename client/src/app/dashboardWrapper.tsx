fc1e3a3334668efe85684aaf28353e467e86c315
Sayam Alvi <sayamalvi07@gmail.com>
2024-10-30 15:52:22 +0530

    reduxt toolkit setup and basic layout

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './(components)/Navbar'
+import Sidebar from './(components)/Sidebar'
+import StoreProvider from './redux'
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+            <Sidebar />
+            <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+                <Navbar />
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <StoreProvider>
+            <DashboardLayout>{children}</DashboardLayout>
+        </StoreProvider>
+    )
+}
+
+export default DashboardWrapper
